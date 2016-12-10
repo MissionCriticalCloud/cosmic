dc97d81b17aa6459a807f8bd9bd06f6a0b73e694
Boris Schrijver <bschrijver@schubergphilis.com>
2016-12-10 14:02:05 +0100

    Remove unused classes/interfaces.

--- cosmic-core/services/secondary-storage-server/src/main/java/org/apache/cloudstack/storage/resource/SecondaryStorageResourceHandler.java
+++
@@ -1,9 +0,0 @@
-package org.apache.cloudstack.storage.resource;
-
-import com.cloud.agent.api.Answer;
-import com.cloud.agent.api.Command;
-
-public interface SecondaryStorageResourceHandler {
-
-    Answer executeRequest(Command cmd);
-}
