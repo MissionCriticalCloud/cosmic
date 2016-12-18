f59918693ee485b83f3318be02b37466f19bdb42
Remi Bergsma <github@remi.nl>
2016-12-18 12:04:49 +0100

    Remove listIdps from UI (is deleted and erroring)

--- cosmic-client/src/main/webapp/scripts/cloudStack.js
+++ cosmic-client/src/main/webapp/scripts/cloudStack.js
@@ -169,25 +169,6 @@
                     }
                 });
 
-
-                // Populate IDP list
-                $.ajax({
-                    type: 'GET',
-                    url: createURL('listIdps'),
-                    dataType: 'json',
-                    async: false,
-                    success: function (data, textStatus, xhr) {
-                        if (data && data.listidpsresponse && data.listidpsresponse.idp) {
-                            var idpList = data.listidpsresponse.idp.sort(function (a, b) {
-                                return a.orgName.localeCompare(b.orgName);
-                            });
-                            g_idpList = idpList;
-                        }
-                    },
-                    error: function (xhr) {
-                    }
-                });
-
                 return userValid ? {
                     user: {
                         userid: g_userid,
