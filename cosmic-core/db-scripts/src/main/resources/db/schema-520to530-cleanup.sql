b6a87de3456e0402308950ec78acd35c1bae6455
Remi Bergsma <github@remi.nl>
2017-01-11 19:49:48 +0100

    Upgrade existing Private Gateways to the new format

--- cosmic-core/db-scripts/src/main/resources/db/schema-520to530-cleanup.sql
+++ cosmic-core/db-scripts/src/main/resources/db/schema-520to530-cleanup.sql
@@ -2,3 +2,11 @@
 -- Schema cleanup from 5.2.0 to 5.3.0;
 --;
 
+-- Backup table for later reference
+CREATE TABLE vpc_gateways_pre530 LIKE vpc_gateways;
+INSERT vpc_gateways_pre530 SELECT * FROM vpc_gateways;
+
+-- delete from original
+ALTER TABLE `vpc_gateways` DROP COLUMN `gateway`;
+ALTER TABLE `vpc_gateways` DROP COLUMN `netmask`;
+ALTER TABLE `vpc_gateways` DROP COLUMN `vlan_tag`;
