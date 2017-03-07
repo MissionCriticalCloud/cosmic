060d35242d772a2af7eb57840c0283bcded79e2e
Remi Bergsma <github@remi.nl>
2017-03-07 18:15:20 +0100

    Prevent duplicate entries in cs_guestnetwork

--- cosmic-core/systemvm/patches/debian/config/opt/cloud/bin/cs_guestnetwork.py
+++ cosmic-core/systemvm/patches/debian/config/opt/cloud/bin/cs_guestnetwork.py
@@ -1,22 +1,12 @@
-keys = ['eth1', 'eth2', 'eth3', 'eth4', 'eth5', 'eth6', 'eth7', 'eth8', 'eth9']
 
+def merge(dbag, ip):
+    for dev in dbag:
+        if dev == "id":
+            continue
+        for address in dbag[dev]:
+            if address['router_guest_ip'] == ip['router_guest_ip']:
+                dbag[dev].remove(address)
 
-def merge(dbag, gn):
-    device = gn['device']
-
-    if not gn['add'] and device in dbag:
-
-        if dbag[device]:
-            device_to_die = dbag[device][0]
-            try:
-                dbag[device].remove(device_to_die)
-            except ValueError, e:
-                print "[WARN] cs_guestnetwork.py :: Error occurred removing item from databag. => %s" % device_to_die
-                del (dbag[device])
-        else:
-            del (dbag[device])
-
-    else:
-        dbag.setdefault(device, []).append(gn)
+    dbag.setdefault('eth' + str(ip['nic_dev_id']), []).append(ip)
 
     return dbag
