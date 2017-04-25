0b723e8f2b51d530011ab28f309684153fbecd0e
Elias Gomes <egomes@schubergphilis.com>
2017-04-25 10:26:54 +0200

    Split 'test_routers_iptables_default_policy.py' into 'test_router_ip_tables_policies.py' and 'test_vpc_ip_tables_policies.py'

--- cosmic-core/test/integration/smoke/test_vpc_ip_tables_policies.py
+++ cosmic-core/test/integration/smoke/test_vpc_ip_tables_policies.py
@@ -1,30 +1,35 @@
-import logging
 import socket
+import logging
 import time
 
 from nose.plugins.attrib import attr
-
 from marvin.cloudstackTestCase import cloudstackTestCase
-from marvin.lib.base import (stopRouter,
-                             startRouter,
-                             destroyRouter,
-                             Account,
-                             VpcOffering,
-                             VPC,
-                             ServiceOffering,
-                             NATRule,
-                             NetworkACL,
-                             PublicIPAddress,
-                             NetworkOffering,
-                             Network,
-                             VirtualMachine)
-from marvin.lib.common import (get_domain,
-                               get_zone,
-                               get_template,
-                               list_routers,
-                               list_hosts)
-from marvin.lib.utils import (cleanup_resources,
-                              get_process_status)
+
+from marvin.lib.base import (
+    PublicIPAddress,
+    VirtualMachine,
+    Network,
+    NetworkOffering,
+    NetworkACL,
+    NATRule,
+    VPC,
+    VpcOffering,
+    ServiceOffering,
+    Account,
+    startRouter,
+    destroyRouter
+)
+from marvin.lib.common import (
+    list_routers,
+    list_hosts,
+    get_template,
+    get_zone,
+    get_domain
+)
+from marvin.lib.utils import (
+    get_process_status,
+    cleanup_resources
+)
 
 
 class Services:
@@ -282,121 +287,6 @@ class TestVPCIpTablesPolicies(cloudstackTestCase):
 
         for router in routers:
             if not router.isredundantrouter and router.vpcid:
-                hosts = list_hosts(
-                    self.apiclient,
-                    id=router.hostid)
-                self.assertEqual(
-                    isinstance(hosts, list),
-                    True,
-                    "Check for list hosts response return valid data")
-
-                host = hosts[0]
-                host.user = self.services["configurableData"]["host"]["username"]
-                host.passwd = self.services["configurableData"]["host"]["password"]
-                host.port = self.services["configurableData"]["host"]["port"]
-                tables = [self.services["configurableData"]["input"], self.services["configurableData"]["forward"]]
-
-                for table in tables:
-                    try:
-                        result = get_process_status(
-                            host.ipaddress,
-                            host.port,
-                            host.user,
-                            host.passwd,
-                            router.linklocalip,
-                            'iptables -L %s' % table)
-                    except KeyError:
-                        self.skipTest(
-                            "Provide a marvin config file with host\
-                                    credentials to run %s" %
-                            self._testMethodName)
-
-                    self.logger.debug("iptables -L %s: %s" % (table, result))
-                    res = str(result)
-
-                    self.assertEqual(
-                        res.count("DROP"),
-                        1,
-                        "%s Default Policy should be DROP" % table)
-
-
-class TestRouterIpTablesPolicies(cloudstackTestCase):
-    @classmethod
-    def setUpClass(cls):
-        # We want to fail quicker if it's failure
-        socket.setdefaulttimeout(60)
-
-        cls.testClient = super(TestRouterIpTablesPolicies, cls).getClsTestClient()
-        cls.apiclient = cls.testClient.getApiClient()
-
-        cls.services = Services().services
-        # Get Zone, Domain and templates
-        cls.domain = get_domain(cls.apiclient)
-        cls.zone = get_zone(cls.apiclient, cls.testClient.getZoneForTests())
-        cls.template = get_template(
-            cls.apiclient,
-            cls.zone.id,
-            cls.services["ostype"])
-
-        cls.services["virtual_machine"]["zoneid"] = cls.zone.id
-        cls.services["virtual_machine"]["template"] = cls.template.id
-
-        cls.account = Account.create(
-            cls.apiclient,
-            cls.services["account"],
-            admin=True,
-            domainid=cls.domain.id)
-
-        cls.service_offering = ServiceOffering.create(
-            cls.apiclient,
-            cls.services["service_offering"])
-
-        cls.logger = logging.getLogger('TestRouterIpTablesPolicies')
-        cls.stream_handler = logging.StreamHandler()
-        cls.logger.setLevel(logging.DEBUG)
-        cls.logger.addHandler(cls.stream_handler)
-
-        cls.entity_manager = EntityManager(cls.apiclient, cls.services, cls.service_offering, cls.account, cls.zone, cls.logger)
-
-        cls._cleanup = [cls.service_offering, cls.account]
-        return
-
-    @classmethod
-    def tearDownClass(cls):
-        try:
-            cleanup_resources(cls.apiclient, cls._cleanup)
-        except Exception as e:
-            raise Exception("Warning: Exception during cleanup : %s" % e)
-        return
-
-    def setUp(self):
-        self.cleanup = []
-        self.entity_manager.set_cleanup(self.cleanup)
-        return
-
-    def tearDown(self):
-        try:
-            cleanup_resources(self.apiclient, self.cleanup)
-        except Exception as e:
-            raise Exception("Warning: Exception during cleanup : %s" % e)
-        return
-
-    @attr(tags=["advanced", "intervlan"], required_hardware="true")
-    def test_02_routervm_iptables_policies(self):
-        """ Test iptables default INPUT/FORWARD policy on RouterVM """
-
-        self.logger.debug("Starting test_02_routervm_iptables_policies")
-
-        vm1 = self.entity_manager.deployvm()
-
-        routers = self.entity_manager.query_routers()
-
-        self.assertEqual(
-            isinstance(routers, list), True,
-            "Check for list routers response return valid data")
-
-        for router in routers:
-            if not router.isredundantrouter and not router.vpcid:
                 hosts = list_hosts(
                     self.apiclient,
                     id=router.hostid)
