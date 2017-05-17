db19ffa08965957214ba84dff6ac1e9908150724
Remi Bergsma <github@remi.nl>
2017-05-17 11:19:32 +0200

    Improve message when automatic volume migration is disabled

--- cosmic-core/engine/orchestration/src/main/java/com/cloud/engine/orchestration/VolumeOrchestrator.java
+++ cosmic-core/engine/orchestration/src/main/java/com/cloud/engine/orchestration/VolumeOrchestrator.java
@@ -1117,7 +1117,9 @@ public class VolumeOrchestrator extends ManagerBase implements VolumeOrchestrati
                                     final VolumeTask task = new VolumeTask(VolumeTaskType.MIGRATE, vol, assignedPool);
                                     tasks.add(task);
                                 } else {
-                                    throw new CloudRuntimeException("Cannot migrate volumes. Volume Migration is disabled");
+                                    throw new CloudRuntimeException("Cannot start VM on the hypervisor it was last running on, due to not enough capacity. Please try to start on " +
+                                            "another hypervisor in the same cluster, or migrate the volumes to another storage pool. Automatic Volume Migration is disabled, " +
+                                            "so this is not handled automatically.");
                                 }
                             }
                         } else {
