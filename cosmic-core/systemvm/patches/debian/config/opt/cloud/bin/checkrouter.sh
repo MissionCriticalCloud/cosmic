85479ae8fea75dd0305a31020ec5cb54ecabafa5
Boris Schrijver <bschrijver@schubergphilis.com>
2017-05-17 19:05:49 +0200

    Removed unsued checkrouter.sh.templ and correct/simplify checkrouter.sh

--- cosmic-core/systemvm/patches/debian/config/opt/cloud/bin/checkrouter.sh
+++ cosmic-core/systemvm/patches/debian/config/opt/cloud/bin/checkrouter.sh
@@ -1,21 +1,20 @@
 #!/usr/bin/env bash
 
-
 STATUS=UNKNOWN
 ROUTER_TYPE=$(cat /etc/cloudstack/cmdline.json | grep type | awk '{print $2;}' | sed -e 's/[,\"]//g')
 if [ "$ROUTER_TYPE" = "router" ]
 then
-	ROUTER_STATE=$(ip addr | grep eth2 | grep state | awk '{print $9;}' | xargs bash -c 'if [ \"$0\" == \"UP\" ]; then echo \"MASTER\"; else echo \"BACKUP\"; fi')
-	STATUS=$ROUTER_STATE
+    ROUTER_STATE=$(ip addr | grep eth2 | grep state | awk '{print $9;}')
 else
-	ROUTER_STATE=$(ip addr | grep eth1 | grep state | awk '{print $9;}')
-	if [ "$ROUTER_STATE" = "UP" ]
-	then
-	    STATUS=MASTER
-	elif [ "$ROUTER_STATE" = "DOWN" ]
-	then
-	    STATUS=BACKUP
-	fi
+    ROUTER_STATE=$(ip addr | grep eth1 | grep state | awk '{print $9;}')
+fi
+
+if [ "$ROUTER_STATE" = "UP" ]
+then
+    STATUS=MASTER
+elif [ "$ROUTER_STATE" = "DOWN" ]
+then
+    STATUS=BACKUP
 fi
 
 echo "Status: ${STATUS}"
