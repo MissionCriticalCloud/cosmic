3c3b4693bd760fd4efc281ecde0bcab2cb7787a3
Boris Schrijver <bschrijver@schubergphilis.com>
2017-06-13 00:18:38 +0200

    Remove unused CertificateVO and CertificateDAO

--- cosmic-core/engine/schema/src/main/java/com/cloud/certificate/dao/CertificateDao.java
+++
@@ -1,8 +0,0 @@
-package com.cloud.certificate.dao;
-
-import com.cloud.certificate.CertificateVO;
-import com.cloud.utils.db.GenericDao;
-
-public interface CertificateDao extends GenericDao<CertificateVO, Long> {
-    public Long persistCustomCertToDb(String certStr, CertificateVO cert, Long managementServerId);
-}
