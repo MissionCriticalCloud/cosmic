02a7db073ac5189a24aa37f2c7ca98e222620a73
Boris Schrijver <bschrijver@schubergphilis.com>
2017-08-02 17:30:45 +0200

    Remove the old cosmic-usage
    This has been superseded by https://github.com/MissionCriticalCloud/cosmic-microservices

--- cosmic-usage/src/main/java/com/cloud/usage/StorageTypes.java
+++
@@ -1,7 +0,0 @@
-package com.cloud.usage;
-
-public class StorageTypes {
-    public static final int TEMPLATE = 1;
-    public static final int ISO = 2;
-    public static final int SNAPSHOT = 3;
-}
