525735dfc2d4a1137f35616dbe6cc28252743d92
Boris Schrijver <bschrijver@schubergphilis.com>
2017-09-17 18:38:27 +0200

    no newlines

--- cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/get_template_version.sh
+++ cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/get_template_version.sh
@@ -3,4 +3,4 @@
 SYSTEMVM_RELEASE=$(cat /etc/cosmic-release)
 SCRIPT_SHA512SUM=$(cat /opt/cosmic/patch/cloud-scripts.tgz.sha512)
 
-echo "${SYSTEMVM_RELEASE}&${SCRIPT_SHA512SUM}"
+echo -n "${SYSTEMVM_RELEASE}&${SCRIPT_SHA512SUM}"
