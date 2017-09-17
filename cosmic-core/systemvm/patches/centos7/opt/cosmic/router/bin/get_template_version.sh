c8fd01a1dc2bfcad951d6599b69f4926e2aed602
Boris Schrijver <bschrijver@schubergphilis.com>
2017-09-17 18:08:17 +0200

    tmp

--- cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/get_template_version.sh
+++ cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/get_template_version.sh
@@ -1,31 +1,3 @@
 #!/bin/bash
 
-# As the last command send to router before any rules operation, wait until boot up done
-
-__TIMEOUT=240
-__FLAGFILE=/var/cache/cloud/boot_up_done
-done=0
-for i in `seq 1 $((${__TIMEOUT} * 10))`
-do
-    if [ -e ${__FLAGFILE} ]
-    then
-        done=1
-        break
-    fi
-    sleep 0.1
-    if [ $((i % 10)) -eq 0 ]
-    then
-        logger -t cloud "Waiting for VM boot up done for one second"
-    fi
-done
-
-if [ -z ${done} ]
-then
-    # declare we failed booting process
-    echo "Waited 60 seconds but boot up haven't been completed"
-    exit
-fi
-
-release=$(cat /etc/cloudstack-release)
-sig=$(cat /var/cache/cloud/cloud-scripts-signature)
-echo "${release}&${sig}"
+echo "$(cat /etc/cosmic-release)"
