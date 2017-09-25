a8ec7bccb4abf38a236fff104161539991918638
Boris Schrijver <bschrijver@schubergphilis.com>
2017-09-25 10:01:08 +0200

    Actually use the value from the cmdline

--- cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/cs_cmdline.py
+++ cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/cs_cmdline.py
@@ -1,7 +1,5 @@
 def merge(dbag, cmdline):
-    if 'redundant_router' in cmdline['cmd_line']:
-        cmdline['cmd_line']['redundant_router'] = "true"
-    else:
+    if 'redundant_router' not in cmdline['cmd_line']:
         cmdline['cmd_line']['redundant_router'] = "false"
     dbag['config'] = cmdline['cmd_line']
     return dbag
