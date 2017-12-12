98510edc3b89a38e561b62b0d4dfcad30258c54e
Boris Schrijver <bschrijver@schubergphilis.com>
2017-12-12 11:48:46 +0100

    Search for stale VIF attachment when creating new logical switch port and remove it

--- cosmic-core/plugins/network/nicira-nvp/src/main/java/com/cloud/network/nicira/NiciraNvpApi.java
+++ cosmic-core/plugins/network/nicira-nvp/src/main/java/com/cloud/network/nicira/NiciraNvpApi.java
@@ -219,7 +219,6 @@ public class NiciraNvpApi {
      * PUT item given a UUID as key and an item object with the new data
      *
      * @param item
-     * @param uuid
      * @throws NiciraNvpApiException
      */
     private <T> void updateWithUri(final T item, final String uri) throws NiciraNvpApiException {
@@ -243,7 +242,6 @@ public class NiciraNvpApi {
     /**
      * DELETE Security Profile given a UUID as key
      *
-     * @param securityProfileUuid
      * @throws NiciraNvpApiException
      */
     private <T> void delete(final String uuid, final Class<T> clazz) throws NiciraNvpApiException {
@@ -254,7 +252,6 @@ public class NiciraNvpApi {
     /**
      * DELETE Security Profile given a UUID as key
      *
-     * @param securityProfileUuid
      * @throws NiciraNvpApiException
      */
     private void deleteWithUri(final String uri) throws NiciraNvpApiException {
@@ -311,7 +308,6 @@ public class NiciraNvpApi {
     /**
      * DELETE Acl given a UUID as key
      *
-     * @param acl
      * @throws NiciraNvpApiException
      */
     public void deleteAcl(final String aclUuid) throws NiciraNvpApiException {
