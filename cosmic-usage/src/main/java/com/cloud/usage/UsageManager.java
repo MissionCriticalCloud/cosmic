e14b7b9cd0152b05fab7fe6d801adc2249120981
Boris Schrijver <bschrijver@schubergphilis.com>
2017-12-20 20:46:44 +0100

    Remove cosmic-usage module

--- cosmic-usage/src/main/java/com/cloud/usage/UsageManager.java
+++
@@ -1,9 +0,0 @@
-package com.cloud.usage;
-
-import com.cloud.utils.component.Manager;
-
-public interface UsageManager extends Manager {
-    public void scheduleParse();
-
-    public void parse(UsageJobVO job, long startDateMillis, long endDateMillis);
-}
