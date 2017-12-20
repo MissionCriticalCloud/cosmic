cf48dcfc3d86ff3f82c36456431c316d60454ed5
Elias Gomes <egomes@schubergphilis.com>
2017-12-20 21:37:21 +0100

    Better deserialization of API response objects

--- cosmic-core/server/src/main/java/com/cloud/api/ApiSerializerHelper.java
+++ cosmic-core/server/src/main/java/com/cloud/api/ApiSerializerHelper.java
@@ -6,59 +6,66 @@ import org.slf4j.LoggerFactory;
 
 public class ApiSerializerHelper {
     public static final Logger s_logger = LoggerFactory.getLogger(ApiSerializerHelper.class.getName());
-    private static final String token = "/";
+    private static final char SEPARATOR = '/';
+    private static final char JSON_OBJECT_START = '{';
 
-    public static String toSerializedString(final Object result) {
-        if (result != null) {
-            final Class<?> clz = result.getClass();
-            final Gson gson = ApiGsonHelper.getBuilder().create();
-
-            if (result instanceof ResponseObject) {
-                return clz.getName() + token + ((ResponseObject) result).getObjectName() + token + gson.toJson(result);
-            } else {
-                return clz.getName() + token + gson.toJson(result);
-            }
+    public static String toSerializedString(final Object object) {
+        if (object == null) {
+            return null;
         }
-        return null;
+
+        final Class<?> clazz = object.getClass();
+        final Gson gson = ApiGsonHelper.getBuilder().create();
+
+        if (object instanceof ResponseObject) {
+            return clazz.getName() + SEPARATOR + ((ResponseObject) object).getObjectName() + SEPARATOR + gson.toJson(object);
+        } else {
+            return clazz.getName() + SEPARATOR + gson.toJson(object);
+        }
     }
 
-    public static Object fromSerializedString(final String result) {
-        try {
-            if (result != null && !result.isEmpty()) {
-
-                final String[] serializedParts = result.split(token);
-
-                if (serializedParts.length < 2) {
-                    return null;
-                }
-                final String clzName = serializedParts[0];
-                String nameField = null;
-                final String content;
-                if (serializedParts.length == 2) {
-                    content = serializedParts[1];
-                } else {
-                    nameField = serializedParts[1];
-                    final int index = result.indexOf(token + nameField + token);
-                    content = result.substring(index + nameField.length() + 2);
-                }
-
-                final Class<?> clz;
-                try {
-                    clz = Class.forName(clzName);
-                } catch (final ClassNotFoundException e) {
-                    return null;
-                }
-
-                final Gson gson = ApiGsonHelper.getBuilder().create();
-                final Object obj = gson.fromJson(content, clz);
-                if (nameField != null) {
-                    ((ResponseObject) obj).setObjectName(nameField);
-                }
-                return obj;
-            }
+    public static Object fromSerializedString(final String string) {
+        if (string == null || string.trim().isEmpty()) {
             return null;
+        }
+
+        try {
+            final int classNameLimit = string.indexOf(SEPARATOR);
+            if (classNameLimit == -1) {
+                return null;
+            }
+
+            final String className = string.substring(0, classNameLimit);
+            final String afterClassName = string.substring(classNameLimit + 1 , string.length());
+
+            final Class<?> clazz;
+            try {
+                clazz = Class.forName(className);
+            } catch (final ClassNotFoundException e) {
+                return null;
+            }
+
+            final Gson gson = ApiGsonHelper.getBuilder().create();
+
+            final int jsonObjectPosition = afterClassName.indexOf(JSON_OBJECT_START);
+            if (jsonObjectPosition == -1) {
+                return null;
+            }
+            if (jsonObjectPosition == 0) {
+                return gson.fromJson(afterClassName, clazz);
+            }
+
+            final String objectName = afterClassName.substring(0, jsonObjectPosition - 1);
+            final String content = afterClassName.substring(jsonObjectPosition, afterClassName.length());
+            final Object object = gson.fromJson(content, clazz);
+            if (object instanceof ResponseObject) {
+                final ResponseObject responseObject = (ResponseObject) object;
+                responseObject.setObjectName(objectName);
+            }
+
+            return object;
         } catch (final RuntimeException e) {
-            s_logger.error("Caught runtime exception when doing GSON deserialization on: " + result);
+            s_logger.error("Caught runtime exception when deserializing on: " + string);
             throw e;
         }
     }
---
+++ cosmic-core/server/src/test/java/com/cloud/api/ApiSerializerHelperTest.java
@@ -0,0 +1,93 @@
+package com.cloud.api;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import com.cloud.api.response.ExceptionResponse;
+import com.cloud.api.response.NetworkACLItemResponse;
+import com.cloud.api.response.SuccessResponse;
+
+import org.junit.Test;
+
+public class ApiSerializerHelperTest {
+
+    @Test
+    public void testFromSerializedStringSimpleSuccessResponse() {
+        final String string = "com.cloud.api.response.SuccessResponse/null/{\"success\":true}";
+        final Object object = ApiSerializerHelper.fromSerializedString(string);
+
+        assertNotNull(object);
+        assertEquals(SuccessResponse.class, object.getClass());
+
+        final SuccessResponse response = (SuccessResponse) object;
+        assertTrue(response.getSuccess());
+        assertEquals("null", response.getObjectName());
+    }
+
+    @Test
+    public void testFromSerializedStringOtherSuccessResponse() {
+        final String string = "com.cloud.api.response.NetworkACLItemResponse/networkacl/{\"id\":\"00000000-0000-0000-0000-000000000000\",\"protocol\":\"tcp\"," +
+                "\"startport\":\"22\",\"endport\":\"22\",\"traffictype\":\"Ingress\",\"state\":\"Active\",\"cidrlist\":\"10.1.0.0/16,10.2.0.0/16\"," +
+                "\"tags\":[],\"aclid\":\"00000000-0000-0000-0000-000000000000\",\"number\":1,\"action\":\"Allow\",\"fordisplay\":true}";
+        final Object object = ApiSerializerHelper.fromSerializedString(string);
+
+        assertNotNull(object);
+        assertEquals(NetworkACLItemResponse.class, object.getClass());
+
+        final NetworkACLItemResponse response = (NetworkACLItemResponse) object;
+        assertEquals("networkacl", response.getObjectName());
+        assertEquals(string, ApiSerializerHelper.toSerializedString(response));
+    }
+
+    @Test
+    public void testFromSerializedStringSimpleExceptionResponse() {
+        final String string = "com.cloud.api.response.ExceptionResponse/{\"errorcode\":530," +
+                "\"errortext\":\"CloudRuntimeException: A simple runtime exception\"}";
+        final Object object = ApiSerializerHelper.fromSerializedString(string);
+
+        assertNotNull(object);
+        assertEquals(ExceptionResponse.class, object.getClass());
+
+        final ExceptionResponse response = (ExceptionResponse) object;
+        assertEquals(530L, (long) response.getErrorCode());
+        assertEquals("CloudRuntimeException: A simple runtime exception", response.getErrorText());
+    }
+
+    @Test
+    public void testFromSerializedStringSimpleExceptionResponseWithName() {
+        final String string = "com.cloud.api.response.ExceptionResponse/exception/{\"errorcode\":530," +
+                "\"errortext\":\"CloudRuntimeException: A simple runtime exception\"}";
+        final Object object = ApiSerializerHelper.fromSerializedString(string);
+
+        assertNotNull(object);
+        assertEquals(ExceptionResponse.class, object.getClass());
+
+        final ExceptionResponse response = (ExceptionResponse) object;
+        assertEquals("exception", response.getObjectName());
+        assertEquals(530L, (long) response.getErrorCode());
+        assertEquals("CloudRuntimeException: A simple runtime exception", response.getErrorText());
+    }
+
+    @Test
+    public void testFromSerializedStringOtherExceptionResponse() {
+        final String string = "com.cloud.api.response.ExceptionResponse/{\"errorcode\":530," +
+                "\"errortext\":\"CloudRuntimeException: Failed to attach volume vol001 to VM vm001; " +
+                "org.libvirt.LibvirtException: XML error: target 'vdx' duplicated for disk sources " +
+                "'/mnt/00000000-0000-0000-0000-000000000000/00000000-0000-0000-0000-000000000000' and " +
+                "'/mnt/00000000-0000-0000-0000-000000000000/00000000-0000-0000-0000-000000000000'\"}";
+        final Object object = ApiSerializerHelper.fromSerializedString(string);
+
+        assertNotNull(object);
+        assertEquals(ExceptionResponse.class, object.getClass());
+
+        final ExceptionResponse response = (ExceptionResponse) object;
+        assertNull(response.getObjectName());
+        assertEquals(530L, (long) response.getErrorCode());
+        assertEquals("CloudRuntimeException: Failed to attach volume vol001 to VM vm001; " +
+                "org.libvirt.LibvirtException: XML error: target 'vdx' duplicated for disk sources " +
+                "'/mnt/00000000-0000-0000-0000-000000000000/00000000-0000-0000-0000-000000000000' and " +
+                "'/mnt/00000000-0000-0000-0000-000000000000/00000000-0000-0000-0000-000000000000'", response.getErrorText());
+    }
+}
