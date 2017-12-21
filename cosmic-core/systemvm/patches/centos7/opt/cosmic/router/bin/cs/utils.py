6af45345841ec0ceb47da725e2bf2cb81a8bc411
Remi Bergsma <github@remi.nl>
2017-12-21 16:05:15 +0100

    Reimplement Site2Site and RemoteAccess VPN

--- cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/cs/utils.py
+++ cosmic-core/systemvm/patches/centos7/opt/cosmic/router/bin/cs/utils.py
@@ -1,6 +1,7 @@
 import re
-
 import netifaces
+import os
+import sys
 
 tier_interface_name_regex = re.compile('eth*')
 
@@ -21,3 +22,9 @@ def get_interface_id_from_mac_address(mac_address):
 
     if interface_name is not None:
         return re.findall(r'\d+', interface_name)[0]
+
+
+def bool_to_yn(value):
+    if value:
+        return "yes"
+    return "no"
