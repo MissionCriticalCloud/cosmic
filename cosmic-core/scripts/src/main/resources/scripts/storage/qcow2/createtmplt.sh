83629d5e9c6e137bd1a8a91f56ca742f1546428d
Remi Bergsma <github@remi.nl>
2018-03-13 17:45:57 +0100

    No checksums after downloading as it's too heavy
    Will be reimplemented elsewhere

--- cosmic-core/scripts/src/main/resources/scripts/storage/qcow2/createtmplt.sh
+++ cosmic-core/scripts/src/main/resources/scripts/storage/qcow2/createtmplt.sh
@@ -21,27 +21,6 @@ then
    fi
 fi
 
-
-verify_cksum() {
-  digestalgo=""
-  case ${#1} in
-        32) digestalgo="md5sum" ;;
-        40) digestalgo="sha1sum" ;;
-        56) digestalgo="sha224sum" ;;
-        64) digestalgo="sha256sum" ;;
-        96) digestalgo="sha384sum" ;;
-        128) digestalgo="sha512sum" ;;
-        *) echo "Please provide valid cheksum" ; exit 3 ;;
-  esac
-  echo  "$1  $2" | $digestalgo  -c --status
-  #printf "$1\t$2" | $digestalgo  -c --status
-  if [ $? -gt 0 ]
-  then
-    printf "Checksum failed, not proceeding with install\n"
-    exit 3
-  fi
-}
-
 untar() {
   local ft=$(file $1| awk -F" " '{print $2}')
   local basedir=$(dirname $1)
