2109f3b5907dd38ff392f6976e6911d7aa23f14f
Boris Schrijver <bschrijver@schubergphilis.com>
2018-04-12 16:25:39 +0200

    fixup

--- cosmic-core/server/src/test/java/com/cloud/network/firewall/FirewallManagerTest.java
+++ cosmic-core/server/src/test/java/com/cloud/network/firewall/FirewallManagerTest.java
@@ -30,24 +30,10 @@ import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-//@Ignore("Requires database to be set up")
 @RunWith(MockitoJUnitRunner.class)
-//@ContextConfiguration(locations = "classpath:/testContext.xml")
-//@ComponentSetup(managerName="management-server", setupXml="network-mgr-component.xml")
 public class FirewallManagerTest {
     private static final Logger s_logger = LoggerFactory.getLogger(FirewallManagerTest.class);
 
-    //    @Before
-    //    public void setUp() {
-    //        Logger daoLogger = LoggerFactory.getLogger(GenericDaoBase.class);
-    //        Logger cloudLogger = LoggerFactory.getLogger("com.cloud");
-    //
-    //        componentlogger.setLevel(Level.WARN);
-    //        daoLogger.setLevel(Level.ERROR);
-    //        cloudLogger.setLevel(Level.ERROR);
-    //        s_logger.setLevel(Level.INFO);
-    //        super.setUp();
-    //    }
     @Mock
     AccountManager _accountMgr;
     @Mock
@@ -73,9 +59,9 @@ public class FirewallManagerTest {
     @Test
     public void testDetectRulesConflict() {
         final List<FirewallRuleVO> ruleList = new ArrayList<>();
-        final FirewallRuleVO rule1 = spy(new FirewallRuleVO("rule1", 3, 500, "UDP", 1, 2, 1, Purpose.Vpn, null, null, null, null));
-        final FirewallRuleVO rule2 = spy(new FirewallRuleVO("rule2", 3, 1701, "UDP", 1, 2, 1, Purpose.Vpn, null, null, null, null));
-        final FirewallRuleVO rule3 = spy(new FirewallRuleVO("rule3", 3, 4500, "UDP", 1, 2, 1, Purpose.Vpn, null, null, null, null));
+        final FirewallRuleVO rule1 = spy(new FirewallRuleVO("rule1", 3, 500, "UDP", 1, 2, 1, Purpose.Vpn, null, null, null));
+        final FirewallRuleVO rule2 = spy(new FirewallRuleVO("rule2", 3, 1701, "UDP", 1, 2, 1, Purpose.Vpn, null, null, null));
+        final FirewallRuleVO rule3 = spy(new FirewallRuleVO("rule3", 3, 4500, "UDP", 1, 2, 1, Purpose.Vpn, null, null, null));
 
         ruleList.add(rule1);
         ruleList.add(rule2);
@@ -88,9 +74,9 @@ public class FirewallManagerTest {
         when(rule2.getId()).thenReturn(2L);
         when(rule3.getId()).thenReturn(3L);
 
-        final FirewallRule newRule1 = new FirewallRuleVO("newRule1", 3, 500, "TCP", 1, 2, 1, Purpose.PortForwarding, null, null, null, null);
-        final FirewallRule newRule2 = new FirewallRuleVO("newRule2", 3, 1701, "TCP", 1, 2, 1, Purpose.PortForwarding, null, null, null, null);
-        final FirewallRule newRule3 = new FirewallRuleVO("newRule3", 3, 4500, "TCP", 1, 2, 1, Purpose.PortForwarding, null, null, null, null);
+        final FirewallRule newRule1 = new FirewallRuleVO("newRule1", 3, 500, "TCP", 1, 2, 1, Purpose.PortForwarding, null, null, null);
+        final FirewallRule newRule2 = new FirewallRuleVO("newRule2", 3, 1701, "TCP", 1, 2, 1, Purpose.PortForwarding, null, null, null);
+        final FirewallRule newRule3 = new FirewallRuleVO("newRule3", 3, 4500, "TCP", 1, 2, 1, Purpose.PortForwarding, null, null, null);
 
         try {
             firewallMgr.detectRulesConflict(newRule1);
