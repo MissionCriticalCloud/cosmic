9939ecc1e5308723100b0471b2c131f5e52ec828
Boris Schrijver <bschrijver@schubergphilis.com>
2018-05-02 14:08:04 +0200

    Remove unused validateUuid method and xerces dependency

--- cosmic-core/utils/src/main/java/com/cloud/utils/UuidUtils.java
+++ cosmic-core/utils/src/main/java/com/cloud/utils/UuidUtils.java
@@ -1,15 +1,8 @@
 package com.cloud.utils;
 
-import org.apache.xerces.impl.xpath.regex.RegularExpression;
-
 public class UuidUtils {
 
     public final static String first(final String uuid) {
         return uuid.substring(0, uuid.indexOf('-'));
-    }
-
-    public static boolean validateUUID(final String uuid) {
-        final RegularExpression regex = new RegularExpression("[0-9a-fA-F]{8}(?:-[0-9a-fA-F]{4}){3}-[0-9a-fA-F]{12}");
-        return regex.matches(uuid);
     }
 }
