a457ad1f03e4191529d0a4aa6e998b7d25c9d056
Remi Bergsma <github@remi.nl>
2018-07-17 14:35:28 +0200

    osTypeId is supposed to be UUID instead of ID

--- cosmic-core/server/src/main/java/com/cloud/api/query/dao/UserVmJoinDaoImpl.java
+++ cosmic-core/server/src/main/java/com/cloud/api/query/dao/UserVmJoinDaoImpl.java
@@ -167,7 +167,7 @@ public class UserVmJoinDaoImpl extends GenericDaoBase<UserVmJoinVO, Long> implem
         userVmResponse.setPublicIpId(userVm.getPublicIpUuid());
         userVmResponse.setPublicIp(userVm.getPublicIpAddress());
         userVmResponse.setKeyPairName(userVm.getKeypairName());
-        userVmResponse.setOsTypeId(userVm.getGuestOsId());
+        userVmResponse.setOsTypeId(userVm.getGuestOsUuid());
         userVmResponse.setManufacturerString(userVm.getManufacturerString());
         userVmResponse.setOptimiseFor(userVm.getOptimiseFor());
         userVmResponse.setRequiresRestart(userVm.getRequiresRestart());
