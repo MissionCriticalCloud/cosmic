12146820158ca4fdc293a6216922429b9d5cc330
Remi Bergsma <github@remi.nl>
2018-11-28 14:12:06 +0100

    Also check the pre-6.2 keepalived path

--- cosmic-core/systemvm/patches/centos7/opt/cosmic/router/scripts/checkrouter.sh
+++ cosmic-core/systemvm/patches/centos7/opt/cosmic/router/scripts/checkrouter.sh
@@ -3,6 +3,8 @@
 STATUS=UNKNOWN
 if [ -f /opt/cosmic/router/keepalived_state ]; then
     STATUS=$(cat /opt/cosmic/router/keepalived_state)
+# Legacy pre-Cosmic 6.2 path
+elif [ -f /tmp/keepalived_state ]; then
+    STATUS=$(cat /tmp/keepalived_state)
 fi
-
 echo "Status: ${STATUS}"
