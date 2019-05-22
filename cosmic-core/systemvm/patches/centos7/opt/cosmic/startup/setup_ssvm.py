ceffec8a4ff9838b6a0a8886f7c51163f8217d80
Remi Bergsma <github@remi.nl>
2019-05-22 11:31:28 +0200

    Set internal routes for DNS and MGT

--- cosmic-core/systemvm/patches/centos7/opt/cosmic/startup/setup_ssvm.py
+++ cosmic-core/systemvm/patches/centos7/opt/cosmic/startup/setup_ssvm.py
@@ -61,6 +61,8 @@ class SecondaryStorageVM:
         if self.cmdline['setrfc1918routes'] == 'true':
             logging.info("Setting rfc1918 routes")
             Utils(self.cmdline).set_rfc1918_routes()
+        logging.info("Setting local routes")
+        Utils(self.cmdline).set_local_routes()
 
         os.system("systemctl start cosmic-agent")
 
