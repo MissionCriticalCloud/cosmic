80f3abee13e20cd6492b8c4d2db54a865ab4c488
Remi Bergsma <github@remi.nl>
2021-01-28 17:35:25 +0100

    Fix comment

--- cosmic-core/cosmic-flyway/src/main/resources/db/migration/V2_38__nullify_DNS_servers_private.sql
+++ cosmic-core/cosmic-flyway/src/main/resources/db/migration/V2_38__nullify_DNS_servers_private.sql
@@ -1,2 +1,2 @@
---- Nullify DNS servers for Private networks
+-- Nullify DNS servers for Private networks
 UPDATE `networks` SET dns1=NULL, dns2=NULL WHERE guest_type = 'Private' AND removed is NULL;
