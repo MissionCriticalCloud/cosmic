08a41a63e9efe16e0eab1db9e1cb50139557b65a
Alexander Verhaar <sanderv32@gmail.com>
2022-02-17 16:24:07 +0100

    Fix for unittest to load tests as modules

---
+++ cosmic-core/test/integration/tests/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["TestDeleteAccount", "TestIpExclusionList", "TestLoadBalance", "TestPasswordService", "TestPasswordService", "TestPrivateGateway",
+           "TestPublicIpAcl", "TestReadOnlyAccount", "TestReleaseIP", "TestRouterIpTablesPolicies", "TestSSVMs", "TestVmMigration",
+           "TestVPCIpTablesPolicies", "TestVPCRedundancy", "TestVPCNics", "TestVpcVpn"]
